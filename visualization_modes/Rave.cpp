5d572896a4cfd79520d083e7c21d9dea10e4d3ab
monstercock <monstercock@LargePP.com>
2023-06-03 15:42:17 +0300

    change Amplitude thickness, change Spectre to symmetry, change Rave

--- visualization_modes/Rave.cpp
+++ visualization_modes/Rave.cpp
@@ -5,35 +5,31 @@ using namespace std;
 
 Rave::Rave(double sample_rate_audio) 
     : AbstractMode(sample_rate_audio), 
-    CenterCircle(1, 70)
+    CenterCircle(1, 3)
 {
     fft.LazyInit(sample_rate_audio);
     CenterCircle.setFillColor(sf::Color::Black);
-    CenterCircle.setOutlineThickness(4.);
+    CenterCircle.setOutlineThickness(5.);
     CenterCircle.setOutlineColor(sf::Color::Red);
 }
 
 void Rave::update(vector<double>& audio_in) {
     auto audio_spectre = fft.Calculate(audio_in);
     Hz.resize(audio_spectre->size());
-    for (size_t i = 0; i < audio_spectre->size(); ++i) {
-        double re = (*audio_spectre)[i].real();
-        double im = (*audio_spectre)[i].imag();
-        Hz[i] = log(sqrt(re * re + im * im)) * 100;
-    }
+    double re = (*audio_spectre)[20].real();
+    double im = (*audio_spectre)[20].imag();
+    Hz[20] = sqrt(re * re + im * im);
 }
 
 void Rave::draw(sf::RenderWindow& window) {
     // double W = window.getSize().x;
     // double H = window.getSize().y;
-
-    double W = WIDTH ;
-    double H = HEIGHT;
-
-    double radius = Hz[100];
-    CenterCircle.setRadius(radius);
-    CenterCircle.setPosition({W / 2 - CenterCircle.getRadius(), H / 2 - CenterCircle.getRadius()});
-    CenterCircle.setOutlineColor({255, 255, 255, CenterCircle.getRadius() - 100});
+    double R = Hz[20];
+    double normalizedR = 1 / (1 + pow(M_E, -1 * R));
+    
+    CenterCircle.setRadius(log10(max(R, 0.)) * 256);
+    CenterCircle.setPosition({WIDTH / 2 - CenterCircle.getRadius(), HEIGHT / 2 - CenterCircle.getRadius()});
+    CenterCircle.setOutlineColor({255, 255, 255, CenterCircle.getRadius()});
     //CenterCircle.rotate(90 / 55);
     
     window.draw(CenterCircle);
--- visualization_modes/Spectre.cpp
+++ visualization_modes/Spectre.cpp
@@ -13,7 +13,12 @@ void Spectre::update(vector<double>& audio_in) {
     for (size_t i = 0; i < audio_spectre->size(); ++i) {
         double re = (*audio_spectre)[i].real();
         double im = (*audio_spectre)[i].imag();
-        rectH[i] = log(sqrt(re * re + im * im)) * 100;
+        double h = sqrt(re * re + im * im);
+        //double normalizedH = 2 / (2 + pow(M_E, -1 * h));
+        double normalizedH = log10(max(h, 0.));
+        rectH[i] = normalizedH * 250;
+
+        //rectH[i] = min(1000., h);
     }
 }
 
@@ -22,17 +27,27 @@ void Spectre::draw(RenderWindow& window) {
     // sf::Shader effect;
     // effect.loadFromFile("bloom.glsl", sf::Shader::Fragment);
 
-    double W = WIDTH / static_cast<double>(rectH.size());
-    int colorJump = static_cast<int>((rectH.size() - 500) / 256);
-    for (size_t i = 0; i < rectH.size(); ++i) {
-        RectangleShape rect(Vector2f(2, 2));
+    double W = WIDTH / static_cast<double>(rectH.size() - 5000) / 2;
+    int colorJump = static_cast<int>((rectH.size() - 2000) / 256);
+
+    //левая половина
+    for (size_t i = 0; i < (rectH.size() - 5000); ++i) {
+        RectangleShape rect(Vector2f(3.5, 3.5));
+
         rect.setFillColor(Color::White);
-        rect.setPosition(Vector2f(i * W, HEIGHT * 7 / 8 - rectH[i]));
+        //на какую высоту от низа экрана надо обрезать спектр
+        float lowBorder = 130;
+        float rectX = i * W;
+        float rectY = min(static_cast<double>(HEIGHT - lowBorder), HEIGHT * 3 / 4 - rectH[i]);
+        rect.setPosition(Vector2f(rectX, rectY));
         if (i % 2 == 0) {
             rect.setFillColor(sf::Color(i / colorJump, 0 , 255 - i / colorJump));
         } else {
             rect.setFillColor(sf::Color(i / colorJump, 100 , 255 - i / colorJump, 150));
         }
+        RectangleShape rectSym(rect);
+        rectSym.setPosition(WIDTH - rectX, rectY);
         window.draw(rect);
+        window.draw(rectSym);
     }
 }
